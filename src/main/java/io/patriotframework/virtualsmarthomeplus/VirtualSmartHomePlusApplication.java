aa34ae78853f21bf496475083ae5e18fd1ddde8a
Marek Stastny <mstastny@redhat.com>
2022-09-14 15:46:09 +0200

    Add maven project with spring-boot

---
+++ src/main/java/io/patriotframework/virtualsmarthomeplus/VirtualSmartHomePlusApplication.java
@@ -0,0 +1,13 @@
+package io.patriotframework.virtualsmarthomeplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualSmartHomePlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualSmartHomePlusApplication.class, args);
+	}
+
+}
