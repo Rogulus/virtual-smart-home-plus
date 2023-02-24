ab7e83011f3578464097e4dfc89a4e593f9d4bf4
Marek Stastny <mstastny@redhat.com>
2023-02-24 08:59:37 +0100

    Add DeviceControllerGeneralTestBase

---
+++ src/test/java/io/patriotframework/virtualsmarthomeplus/controllers/fireplace/FireplaceControllerGeneralTest.java
@@ -0,0 +1,10 @@
+package io.patriotframework.virtualsmarthomeplus.controllers.fireplace;
+
+import io.patriotframework.virtualsmarthomeplus.controllers.DeviceControllerGeneralTestBase;
+
+public class FireplaceControllerGeneralTest extends DeviceControllerGeneralTestBase {
+
+    protected String getPathToDevices() {
+        return "/api/v0.1/house/device";
+    }
+}
