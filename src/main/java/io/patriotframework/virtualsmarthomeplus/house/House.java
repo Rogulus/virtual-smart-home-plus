9dff9b11ce493ae75b6fb209c7ddbfaa07c674b2
Marek Stastny <mstastny@redhat.com>
2023-02-24 14:01:05 +0100

    Implement House methods

--- src/main/java/io/patriotframework/virtualsmarthomeplus/house/House.java
+++ src/main/java/io/patriotframework/virtualsmarthomeplus/house/House.java
@@ -1,16 +1,28 @@
 package io.patriotframework.virtualsmarthomeplus.house;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import io.patriotframework.virtualsmarthomeplus.house.devices.Device;
-import org.apache.commons.lang3.NotImplementedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 import javax.management.openmbean.KeyAlreadyExistsException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * This class is responsible for management of {@code Device} used in the Virtual Smart Home.
  */
+@Service
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class House {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(House.class);
+    private final Map<String, Device> devices = new ConcurrentHashMap<>();
 
     /**
      * Puts new device to the house object.
@@ -20,18 +32,29 @@ public class House {
      * @throws KeyAlreadyExistsException if device with given label is already present in the house
      */
     public void addDevice(Device device) throws IllegalArgumentException, KeyAlreadyExistsException {
-        throw new NotImplementedException("Not implemented yet.");
+        if(device == null) {
+            throw new IllegalArgumentException("Device cannot be null");
+        }
+        final Device origDevice = devices.putIfAbsent(device.getLabel(), device);
+        if (origDevice != null) {
+            throw new KeyAlreadyExistsException(
+                    String.format("Device can't be added to the house twice. (label: %s)", origDevice.getLabel()));
+        }
+        LOGGER.info(String.format("Device %s with label %s added to the house", device, device.getLabel()));
     }
 
     /**
      * Returns a device occupied in the house.
      *
      * @param label label of the demanded device
-     * @return instance of device with given label
+     * @return instance of device with given label, null if device is not present in the house
      * @throws IllegalArgumentException if label is null
      */
     public Device getDevice(String label) throws IllegalArgumentException {
-        throw new NotImplementedException("Not implemented yet.");
+        if(label == null) {
+            throw new IllegalArgumentException("Label can't be null.");
+        }
+        return devices.get(label);
     }
 
     /**
@@ -39,39 +62,54 @@ public class House {
      *
      * @param device device with updated values (device is specified by its label)
      * @throws IllegalArgumentException if device is null
-     * @throws NoSuchElementException if no device with given label is present in the house
+     * @throws NoSuchElementException if no device with given label is not present in the house
      */
     public void updateDevice(Device device) throws IllegalArgumentException, NoSuchElementException {
-        throw new NotImplementedException("Not implemented yet.");
+        if(device == null)  {
+            throw new IllegalArgumentException("Device can't be null");
+        }
+        final Device origDevice = devices.get(device.getLabel());
+        if(origDevice== null) {
+            throw new NoSuchElementException(
+                    String.format("Device with label: %s is not present in the house", device.getLabel()));
+        }
+        devices.put(device.getLabel(), device);
+        if(!LOGGER.isDebugEnabled()) {
+            LOGGER.info(String.format("Device with label:%s updated.", device.getLabel()));
+        }
+        LOGGER.debug(String.format("Device:%s updated to: %s", origDevice, device));
     }
 
     /**
      * Removes given device from the house. (Removes device with the same label.)
      *
-     * @param device label of the device to be removed
+     * @param label label of the device to be removed
      * @throws IllegalArgumentException if label is null
      * @throws NoSuchElementException if house doesn't contain device with given label
      */
-    public void removeDevice(Device device) throws IllegalArgumentException, NoSuchElementException{
-        throw new NotImplementedException("Not implemented yet.");
+    public void removeDevice(String label) throws IllegalArgumentException, NoSuchElementException{
+        if(label == null) {
+            throw new IllegalArgumentException("label can't be null");
+        }
+        final Device origDevice = devices.remove(label);
+        if(origDevice == null) {
+            throw new NoSuchElementException(
+                    String.format("Device with label: %s can't be removed from the house because it does not contain" +
+                            " such device", label));
+        } else {
+            LOGGER.info(String.format("Device: %s removed from the house", label));
+        }
     }
 
     /**
      * Provides devices of certain type which are stored in house.
      *
      * @param deviceOfType type of requested devices
-     * @return set of devices of requested type, null if such device does not exist
+     * @return set of devices of requested type, Empty set if such device does not exist
      */
-    public HashSet<Device> getDevicesOfType(Class<? extends Device> deviceOfType) {
-        throw new NotImplementedException("Not implemented yet.");
-    }
-
-    /**
-     * Provides the name of the house.
-     *
-     * @return name of the house
-     */
-    public String getName() {
-        throw new NotImplementedException("Not implemented yet.");
+    public HashMap<String, Device> getDevicesOfType(Class<? extends Device> deviceOfType) {
+        return (HashMap<String, Device>) devices.values().stream()
+                .filter(x -> x.getClass().equals(deviceOfType))
+                .collect(Collectors.toMap(Device::getLabel, Function.identity()));
     }
 }
