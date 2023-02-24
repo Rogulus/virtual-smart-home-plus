2d7052b5d801366794b6367459ee57586adc7e89
Marek Stastny <mstastny@redhat.com>
2023-02-24 08:58:31 +0100

    Move mock device

--- src/test/java/io/patriotframework/virtualsmarthomeplus/MockDevice.java
+++ src/test/java/io/patriotframework/virtualsmarthomeplus/MockDevice.java
@@ -1,20 +1,24 @@
-package io.patriotframework.virtualsmarthomeplus.controllers.house;
+package io.patriotframework.virtualsmarthomeplus;
 
 import io.patriotframework.virtualsmarthomeplus.house.devices.Device;
 import io.patriotframework.virtualsmarthomeplus.house.devices.finalDevices.Fireplace;
 import org.apache.commons.lang3.NotImplementedException;
 
-public class TestDevice extends Device {
+public class MockDevice extends Device {
 
-     public TestDevice(String label) {
+     public MockDevice(String label) {
          super(label);
      }
 
     public Device createWithSameState(String newLabel) {
-        return new TestDevice("newLabel");
+        return new MockDevice("newLabel");
     }
 
     public boolean hasSameAttributes(Device device) throws IllegalArgumentException {
         throw new NotImplementedException("Not implemented yet.");
     }
+    public Fireplace createWithSameAttributes(String newLabel) {
+        throw new NotImplementedException("Not implemented yet.");
+    }
 }
+
